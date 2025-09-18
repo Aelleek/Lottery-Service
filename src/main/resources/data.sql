fde76c4f84e2cb9841199fda08d2719763f3b45b
Aelleek <aelleek@gmail.com>
2025-09-18 14:07:27 +0900

    update lotto_record test data in data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -6,6 +6,6 @@
 
 INSERT INTO lotto_record (member_id, numbers, round, recommended_at, manual, purchased, guest, source)
 VALUES
-    (1, '5 12 18 22 34 40', 1123, CURRENT_TIMESTAMP, false, false, false, 'MANUAL'),
+--    (1, '5 12 18 22 34 40', 1123, CURRENT_TIMESTAMP, false, false, false, 'MANUAL'),
 ----    (2, '3 7 15 19 25 44', 1123, CURRENT_TIMESTAMP, false, true, false, 'AUTO'),
     (null, '1 4 9 11 36 42', 1123, CURRENT_TIMESTAMP, false, false, true, 'GUEST');
